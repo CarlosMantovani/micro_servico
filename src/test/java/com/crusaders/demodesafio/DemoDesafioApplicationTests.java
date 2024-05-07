3936432effec71c5a59935e4e183bcf6188b2824
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-05-07 09:42:39 -0300

    Criação projeto

---
+++ src/test/java/com/crusaders/demodesafio/DemoDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crusaders.demodesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
