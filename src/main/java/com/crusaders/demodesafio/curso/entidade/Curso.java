937aea6f9a897f545f83069e9683244fd3b25293
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-08 11:07:06 -0300

    Modificação Curso

--- src/main/java/com/crusaders/demodesafio/curso/entidade/Curso.java
+++ src/main/java/com/crusaders/demodesafio/curso/entidade/Curso.java
@@ -2,29 +2,32 @@ package com.crusaders.demodesafio.curso.entidade;
 
 import com.crusaders.demodesafio.curso.entidade.Enum.Status;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-@Getter @Setter @AllArgsConstructor
+@Table(name = "curso")
+@Entity(name = "curso")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Curso implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    @Column(name = "nome", nullable = false,length = 200)
+    @Column(name = "nome", nullable = false, length = 200)
     private String nome;
-    @Column(name = "qtdHoras",nullable = false,length = 200)
+    @Column(name = "qtdHoras", nullable = false, length = 200)
     private String qtdHoras;
-    @Column(name = "professor",nullable = false)
+    @Column(name = "professor", nullable = false)
     private String professor;
-    @Column(name = "areaConhecimento",nullable = false,length = 200)
+    @Column(name = "areaConhecimento", nullable = false, length = 200)
     private String areaConhecimento;
     @Enumerated(EnumType.STRING)
-    @Column(name = "status",length = 25)
+    @Column(name = "status", length = 25)
     private Status status = Status.ATIVO;
 
     @Override
@@ -42,9 +45,6 @@ public class Curso implements Serializable {
 
     @Override
     public String toString() {
-        return "Curso{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                '}';
+        return "Curso{" + "id=" + id + ", nome='" + nome + '\'' + '}';
     }
 }
