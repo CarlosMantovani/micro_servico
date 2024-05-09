4abe4842ab82cb7488a76080e6daa005230e44a5
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-05-09 13:29:00 -0300

    Criação CursoProfessorDto

---
+++ src/main/java/com/crusaders/demodesafio/curso/web/dto/CursoProfessorDto.java
@@ -0,0 +1,12 @@
+package com.crusaders.demodesafio.curso.web.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CursoProfessorDto {
+    private String nomeProfessor;
+}
