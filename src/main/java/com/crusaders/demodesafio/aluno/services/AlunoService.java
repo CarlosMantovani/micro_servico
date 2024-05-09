84519e0c8c9c19728f05a550dd26ae05d735049b
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-05-09 07:57:47 -0300

    Implementação dos métodos cadastrarAluno e listarAlunos em AlunoService

--- src/main/java/com/crusaders/demodesafio/aluno/services/AlunoService.java
+++ src/main/java/com/crusaders/demodesafio/aluno/services/AlunoService.java
@@ -1,14 +1,26 @@
 package com.crusaders.demodesafio.aluno.services;
 
+import com.crusaders.demodesafio.aluno.entities.Aluno;
 import com.crusaders.demodesafio.aluno.repository.AlunoRepository;
+import com.crusaders.demodesafio.curso.entidade.Curso;
+import com.crusaders.demodesafio.curso.repository.CursoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AlunoService {
 
+    @Autowired
     private final AlunoRepository alunoRepository;
 
+    public Aluno cadastrarAluno(Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    public Iterable<Aluno> listarAlunos() {
+        return alunoRepository.findAll();
+    }
 
 }
