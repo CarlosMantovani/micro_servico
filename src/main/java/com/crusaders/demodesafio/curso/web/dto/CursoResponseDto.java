355585187284a56c5d3f358417f44e31168d169e
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-05-09 13:17:25 -0300

    Criação do CursoMapper e CursoResponseDto e alteração da dependencia

---
+++ src/main/java/com/crusaders/demodesafio/curso/web/dto/CursoResponseDto.java
@@ -0,0 +1,22 @@
+
+package com.crusaders.demodesafio.curso.web.dto;
+
+
+
+import com.crusaders.demodesafio.Enum.Status;
+import lombok.*;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    public class CursoResponseDto {
+        private Long id;
+        private String nome;
+        private String qtdHoras;
+        private String areaConhecimento;
+        private String professor;
+        private Status status;
+
+    }
