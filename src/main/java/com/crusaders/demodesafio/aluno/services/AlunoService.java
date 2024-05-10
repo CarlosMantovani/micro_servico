c5e85d49c617cfdde8167fa2cb1333cdf0d6296b
gaa.vieira95@gmail.com <gaa.vieira95@gmail.com>
2024-05-10 10:17:45 -0300

    Criado o metodo buscarPorId e alterarStatusAluno

--- src/main/java/com/crusaders/demodesafio/aluno/services/AlunoService.java
+++ src/main/java/com/crusaders/demodesafio/aluno/services/AlunoService.java
@@ -1,9 +1,9 @@
 package com.crusaders.demodesafio.aluno.services;
 
+import com.crusaders.demodesafio.Enum.Status;
 import com.crusaders.demodesafio.aluno.entities.Aluno;
 import com.crusaders.demodesafio.aluno.repository.AlunoRepository;
-import com.crusaders.demodesafio.curso.entidade.Curso;
-import com.crusaders.demodesafio.curso.repository.CursoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +21,17 @@ public class AlunoService {
 
     public Iterable<Aluno> listarAlunos() {
         return alunoRepository.findAll();
+    }
+
+    public Aluno buscarPorId(Long id) {
+        return alunoRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Usuário id=%s não encontrado", id))
+        );
+    }
+    public Aluno alterarStatusAluno(Long id, Status status) {
+        Aluno aluno = buscarPorId(id);
+        aluno.setStatus(status);
+        return alunoRepository.save(aluno);
     }
 
 }
