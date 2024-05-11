6024fae9ebee37dc44ebeae2851e046b447729e5
gaa.vieira95@gmail.com <gaa.vieira95@gmail.com>
2024-05-11 16:30:33 -0300

    Implementação da funcionalidade das matriculas dos alunos.

--- src/main/java/com/crusaders/demodesafio/alunoematricula/web/controller/MatriculaController.java
+++ src/main/java/com/crusaders/demodesafio/alunoematricula/web/controller/MatriculaController.java
@@ -4,9 +4,11 @@ import com.crusaders.demodesafio.alunoematricula.entities.Aluno;
 import com.crusaders.demodesafio.alunoematricula.services.AlunoService;
 import com.crusaders.demodesafio.alunoematricula.entities.Matricula;
 import com.crusaders.demodesafio.alunoematricula.services.MatriculaService;
+import com.crusaders.demodesafio.alunoematricula.web.dto.MatriculaAlunoDto;
 import com.crusaders.demodesafio.alunoematricula.web.dto.MatriculaRequestDto;
 import com.crusaders.demodesafio.alunoematricula.web.dto.MatriculaResponseDto;
 import com.crusaders.demodesafio.alunoematricula.web.dto.mapper.AlunoMapper;
+import com.crusaders.demodesafio.alunoematricula.web.dto.mapper.MatriculaMapper;
 import com.crusaders.demodesafio.curso.entidade.Curso;
 import com.crusaders.demodesafio.curso.service.CursoService;
 import com.crusaders.demodesafio.curso.web.dto.mapper.CursoMapper;
@@ -28,7 +30,7 @@ public class MatriculaController {
     private final CursoService cursoService;
 
     @PostMapping
-    public ResponseEntity<MatriculaResponseDto> matricularAluno(@RequestBody MatriculaRequestDto requestDto) {
+    public ResponseEntity<Void> matricularAluno(@RequestBody MatriculaRequestDto requestDto) {
         Curso curso = cursoService.buscarPorId(requestDto.getCursoId());
         Aluno aluno = alunoService.buscarPorId(requestDto.getAlunoId());
 
@@ -39,10 +41,7 @@ public class MatriculaController {
         boolean matriculado = matriculaService.matricularAluno(curso, aluno);
 
         if (matriculado) {
-            List<Aluno> alunosMatriculados = matriculaService.findByCurso(curso).stream()
-                    .map(Matricula::getAluno)
-                    .toList();
-
+            List<Aluno> alunosMatriculados = matriculaService.listarAlunosMatriculados(curso);
             int totalAlunos = alunosMatriculados.size();
 
             MatriculaResponseDto responseDto = new MatriculaResponseDto();
@@ -50,9 +49,22 @@ public class MatriculaController {
             responseDto.setAlunos(alunosMatriculados.stream().map(AlunoMapper::toDto).collect(Collectors.toList()));
             responseDto.setTotalAlunos(totalAlunos);
 
-            return ResponseEntity.ok(responseDto);
+            return ResponseEntity.noContent().build();
+
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
+    }
+
+    @GetMapping("/aluno/{alunoId}")
+    public ResponseEntity<List<MatriculaAlunoDto>> listarMatriculasPorAluno(@PathVariable Long alunoId) {
+        List<MatriculaAlunoDto> responseDtoList = matriculaService.listarMatriculasPorAluno(alunoId);
+        return ResponseEntity.ok(responseDtoList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MatriculaAlunoDto> getById(@PathVariable Long id) {
+        Matricula matricula = matriculaService.buscarPorId(id);
+        return ResponseEntity.ok(MatriculaMapper.toDtoId(matricula));
     }
 }
