793cff3f0dfbb7d5aa478be83676d4e4693366d0
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-13 11:00:03 -0300

    Fix: MatriculaRepository

--- src/main/java/com/crusaders/demodesafio/alunoematricula/repository/MatriculaRepository.java
+++ src/main/java/com/crusaders/demodesafio/alunoematricula/repository/MatriculaRepository.java
@@ -4,9 +4,11 @@ import com.crusaders.demodesafio.Enum.Status;
 import com.crusaders.demodesafio.alunoematricula.entities.Matricula;
 import com.crusaders.demodesafio.curso.entidade.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
     long countByCursoAndStatus(Curso curso, Status status);
     List<Matricula> findByCurso(Curso curso);
